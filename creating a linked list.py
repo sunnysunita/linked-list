350d15690329468b21a00fa689935c5c97e7c946
sunnysunita <sunnysunita.com@gmail.com>
2020-04-08 22:47:16 +0530

    final

--- creating a linked list.py
+++ creating a linked list.py
@@ -4,7 +4,44 @@ class Node:
         self.next = None
 
 
-a = Node(10)
+# we need to create a linked list which take input from user and return head node
+
+def take_input():
+    user_input = input("enter list of number separated by spaces: ")
+    list_num = [int(e) for e in user_input.split(" ")]
+    head = None
+    for e in list_num:
+        if e == -1:
+            break
+        new_node = Node(e)
+        if head is None:
+            head = new_node
+        else:
+            curr=head
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = new_node
+    return head
+
+
+
+
+head = take_input()
+print(head.next.data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""a = Node(10)
 b = Node(20)
 print(a.data)
 print(b.data)
@@ -12,3 +49,4 @@ a.next = b  # referencing node a with b
 
 print(a.next.data)
 # print(b.next.data)
+"""
